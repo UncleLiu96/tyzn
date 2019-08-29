7700df4f7f53121525f37484ea40c6a2b9e83c91
nanfonjin@foxmail.com <nanfonjin@foxmail.com>
2019-08-29 16:13:03 +0800

    pojo

--- src/test/java/com/tyzn/TyznApplicationTests.java
+++ src/test/java/com/tyzn/TyznApplicationTests.java
@@ -1,16 +1,20 @@
 package com.tyzn;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TyznApplicationTests {
 
+
     @Test
     public void contextLoads() {
+
     }
 
 }

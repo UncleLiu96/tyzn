9ec80570c8d2020a075e5316f8ca7fc85afbadf7
nanfonjin@foxmail.com <nanfonjin@foxmail.com>
2019-08-29 14:40:36 +0800

    1

---
+++ src/main/java/com/tyzn/TyznApplication.java
@@ -0,0 +1,13 @@
+package com.tyzn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyznApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TyznApplication.class, args);
+    }
+
+}

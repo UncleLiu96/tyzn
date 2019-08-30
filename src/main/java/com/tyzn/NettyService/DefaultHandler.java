d9ba1d4949a04d78b1f18b6ec1a3003f1a6fbff6
吴朝鹏 <864639946@qq.com>
2019-08-30 17:57:19 +0800

    添加Netty配置类，处理类

---
+++ src/main/java/com/tyzn/NettyService/DefaultHandler.java
@@ -0,0 +1,83 @@
+package com.tyzn.NettyService;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.timeout.IdleStateEvent;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 通信处理类，处理连接，断开，接收消息，心跳，异常等。
+ */
+@Slf4j
+public class DefaultHandler extends SimpleChannelInboundHandler<Object> {
+
+
+    /**
+     *
+     * @param ctx
+     * @param cause
+     * @throws Exception
+     * 出现异常处理
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        //出现异常，断开连接
+        log.info("出现异常，关闭连接！");
+        ctx.close();
+    }
+
+    /**
+     *
+     * @param ctx
+     * @param evt
+     * @throws Exception
+     * 心跳处理
+     */
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            IdleStateEvent e = (IdleStateEvent) evt;
+            switch (e.state()) {
+                case READER_IDLE:   //读
+
+                    break;
+                case WRITER_IDLE:   //写
+
+                    break;
+                case ALL_IDLE:  //读写都有
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    /**
+     * @param ctx
+     * @throws Exception
+     * 连接断开处理
+     */
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        log.info("连接关闭，删除客户端信息");
+    }
+
+    /**
+     * @param ctx
+     * @throws Exception
+     * 连接建立处理
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        log.info("连接成功，保存客户端信息");
+    }
+
+    /**
+     * 收到消息处理
+     */
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
+        log.info("收到消息");
+    }
+}
---
+++ src/main/java/com/tyzn/NettyService/InitHandler.java
@@ -0,0 +1,21 @@
+package com.tyzn.NettyService;
+
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.timeout.IdleStateHandler;
+
+public class InitHandler {
+
+    protected void initHandler(ChannelPipeline ch, InitBean initBean) {
+
+        /*编解码方式，后续根据协议，自己定义编解码方式*/
+        ch.addLast("stringDecode",new StringDecoder());
+        ch.addLast("stringEncode",new StringEncoder());
+        /*心跳设定*/
+        ch.addLast(new IdleStateHandler(initBean.getHeart(),0,0));
+        /*通信处理类，处理连接，断开，收到信息等处理*/
+        ch.addLast(new DefaultHandler());
+
+    }
+}

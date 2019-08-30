7cd3d81aa1e05fd15b3ffe908bb939a1d8290841
吴朝鹏 <864639946@qq.com>
2019-08-30 15:36:12 +0800

    添加依赖

---
+++ src/main/java/com/tyzn/NettyService/NettyServerBootstrap.java
@@ -0,0 +1,114 @@
+package com.tyzn.NettyService;
+
+import com.tyzn.Utils.IpUtils;
+import com.tyzn.Utils.RemotingUtil;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollServerSocketChannel;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+public class NettyServerBootstrap {
+
+    private InitBean serverBean;
+
+    public InitBean getServerBean() {
+        return serverBean;
+    }
+
+    public void setServerBean(InitBean serverBean) {
+        this.serverBean = serverBean;
+    }
+
+    private EventLoopGroup bossGroup;
+
+    private EventLoopGroup workGroup;
+
+    ServerBootstrap bootstrap=null ;
+
+    /**
+     * 开启服务器
+     */
+    public void start(){
+        initEventPool();
+        bootstrap.group(bossGroup,workGroup)
+                .channel(useEpoll()? EpollServerSocketChannel.class: NioServerSocketChannel.class)
+                .option(ChannelOption.SO_REUSEADDR, serverBean.isReuseaddr())
+                .option(ChannelOption.SO_BACKLOG, serverBean.getBacklog())
+                .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                .option(ChannelOption.SO_RCVBUF, serverBean.getRevbuf())
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        //加载参数处理类
+                        //initHandler(ch.pipeline(),serverBean);
+                    }
+                })
+                .childOption(ChannelOption.TCP_NODELAY, serverBean.isTcpNodelay())
+                .childOption(ChannelOption.SO_KEEPALIVE, serverBean.isKeepalive())
+                .childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
+        bootstrap.bind(IpUtils.getHost(),serverBean.getPort()).addListener((ChannelFutureListener) channelFuture -> {
+            if (channelFuture.isSuccess())
+                log.info("服务端启动成功【" + IpUtils.getHost() + ":" + serverBean.getPort() + "】");
+            else
+                log.info("服务端启动失败【" + IpUtils.getHost() + ":" + serverBean.getPort() + "】");
+        });
+    }
+
+    /**
+     * 是否是Linux系统
+     */
+    private boolean useEpoll() {
+        return RemotingUtil.isLinuxPlatform();
+    }
+
+    /**
+     * 初始化EnentPool 参数，初始化启动对象
+     */
+    private void  initEventPool(){
+        bootstrap= new ServerBootstrap();
+        if(useEpoll()){
+            bossGroup = new EpollEventLoopGroup(serverBean.getBossThread(), new ThreadFactory() {
+                private AtomicInteger index = new AtomicInteger(0);
+
+                public Thread newThread(Runnable r) {
+                    return new Thread(r, "LINUX_BOSS_" + index.incrementAndGet());
+                }
+            });
+            workGroup = new EpollEventLoopGroup(serverBean.getWorkThread(), new ThreadFactory() {
+                private AtomicInteger index = new AtomicInteger(0);
+
+                public Thread newThread(Runnable r) {
+                    return new Thread(r, "LINUX_WORK_" + index.incrementAndGet());
+                }
+            });
+
+        }
+        else {
+            bossGroup = new NioEventLoopGroup(serverBean.getBossThread(), new ThreadFactory() {
+                private AtomicInteger index = new AtomicInteger(0);
+
+                public Thread newThread(Runnable r) {
+                    return new Thread(r, "BOSS_" + index.incrementAndGet());
+                }
+            });
+            workGroup = new NioEventLoopGroup(serverBean.getWorkThread(), new ThreadFactory() {
+                private AtomicInteger index = new AtomicInteger(0);
+
+                public Thread newThread(Runnable r) {
+                    return new Thread(r, "WORK_" + index.incrementAndGet());
+                }
+            });
+        }
+    }
+}

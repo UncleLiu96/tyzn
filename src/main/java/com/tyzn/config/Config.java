37ae43c0e99b7311b75381fb01a5dd99fd2c81e8
nanfonjin@foxmail.com <nanfonjin@foxmail.com>
2019-09-02 16:53:33 +0800

    common

---
+++ src/main/java/com/tyzn/config/Config.java
@@ -0,0 +1,15 @@
+package com.tyzn.config;
+
+import com.tyzn.NettyService.NettyServerBootstrap;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Unclue_liu
+ * @organization tyzn
+ * @date 2019/9/2 0002 14:49
+ * @desc TODO
+ */
+@Component
+public class Config {
+
+}
